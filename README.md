29ce9a53d779ecc9e69ffd7684008b5a8a0cb52e
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-11-25 17:01:14 -0300

    Realizando a conexão com o banco usando dotenv

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # exemplo-api
+
+Exemplo de arquivo .env
+
+```
+POSTGRES_USER = usuario
+POSTGRES_PASSWORD = senha
+POSTGRES_HOST = localhost
+POSTGRES_PORT = 5432
+POSTGRES_DB = banco
+```
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import pg from 'pg'
 const { Client } = pg
 const client = new Client({
-    user: 'postgres',
-    password: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'aula'
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB
 });
 
 conectar();

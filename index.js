00fb3970fbf579b9826a744b686bb446a13f6295
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-11-27 14:00:57 -0300

    Adicionando exemplo para svg

--- index.js
+++ index.js
@@ -21,8 +21,19 @@ async function conectar(){
 const app = express();
 app.use(express.json());
 
-app.get('/svg', async (req, res) => { 
-    console.log('Hello world!');
+app.get('/svg/:estado/:municipio', async (req, res) => { 
+    let estado = req.params.estado;
+    let municipio = req.params.municipio;
+    let pathEstado = await client.query('SELECT ST_AsSVG(geom) FROM estado WHERE nome ilike $1',[estado]);
+    let pathMunicipio = await client.query('SELECT ST_AsSVG(geom) FROM municipio WHERE nome ilike $1',[municipio]);
+    let viewBox = await client.query('SELECT getViewBox($1)',[estado]);
+
+    res.json({
+        pathestado: pathEstado.rows[0].st_assvg,
+        pathmunicipio: pathMunicipio.rows[0].st_assvg,
+        viewBox: viewBox.rows[0].getviewbox
+    });
+
 });
 
 app.listen(3000, () =>{

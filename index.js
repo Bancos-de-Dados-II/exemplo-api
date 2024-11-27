0bdc3aac25a01cce31a5f2c2c9956bfbcb19828f
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-11-27 13:34:09 -0300

    Adicionando express

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import dotenv from 'dotenv';
+import express from 'express';
 dotenv.config();
-import pg from 'pg'
-const { Client } = pg
+import pg from 'pg';
+
+const { Client } = pg;
 const client = new Client({
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
@@ -13,14 +15,16 @@ const client = new Client({
 conectar();
 
 async function conectar(){
-    await client.connect()
-
-    try {
-        const res = await client.query('SELECT $1::text as message', ['Hello world!'])
-        console.log(res.rows[0].message) // Hello world!
-    } catch (err) {
-        console.error(err);
-    } finally {
-        await client.end()
-    }
+    await client.connect();
 }
+
+const app = express();
+app.use(express.json());
+
+app.get('/svg', async (req, res) => { 
+    console.log('Hello world!');
+});
+
+app.listen(3000, () =>{
+    console.log('Aplicação rodando na porta 3000');
+});

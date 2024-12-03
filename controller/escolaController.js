b3f5778e9826a17aabfa6216723e2a3be763cdea
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-12-03 14:30:27 -0300

    Adicionando rota para deletar

--- controller/escolaController.js
+++ controller/escolaController.js
@@ -13,3 +13,13 @@ export async function criarEscola(req, res){
         res.status(400).send(err.errors[0].message);
     }
 }
+
+export async function deletarEscola(req, res){
+    const escola = await Escola.findByPk(req.params.id);
+    if(!escola){
+        res.status(404).send("Escola não encontrada");
+        return;
+    }
+    await escola.destroy();
+    res.json(escola);
+}
--- router/escolaRouter.js
+++ router/escolaRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { listarEscolas, criarEscola } from '../controller/escolaController.js';
+import { listarEscolas, criarEscola, deletarEscola } from '../controller/escolaController.js';
 
 const escolaRouter = express.Router();
 
 escolaRouter.get('/', listarEscolas);
 escolaRouter.post('/', criarEscola);
+escolaRouter.delete('/:id', deletarEscola);
 
 export default escolaRouter;

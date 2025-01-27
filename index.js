c8fdbe6fdc44a5f59e2e8107f55419be8574816c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-01-27 16:18:59 -0300

    Adicionando arquivo para conexão com o redis

--- index.js
+++ index.js
@@ -11,18 +11,3 @@ app.use('/escolas', escolaRouter);
 app.listen(3000, () =>{
     console.log('Aplicação rodando na porta 3000');
 });
-
-// app.get('/svg/:estado/:municipio', async (req, res) => { 
-//     let estado = req.params.estado;
-//     let municipio = req.params.municipio;
-//     let pathEstado = await client.query('SELECT ST_AsSVG(geom) FROM estado WHERE nome ilike $1',[estado]);
-//     let pathMunicipio = await client.query('SELECT ST_AsSVG(geom) FROM municipio WHERE nome ilike $1',[municipio]);
-//     let viewBox = await client.query('SELECT getViewBox($1)',[estado]);
-
-//     res.json({
-//         pathestado: pathEstado.rows[0].st_assvg,
-//         pathmunicipio: pathMunicipio.rows[0].st_assvg,
-//         viewBox: viewBox.rows[0].getviewbox
-//     });
-
-// });

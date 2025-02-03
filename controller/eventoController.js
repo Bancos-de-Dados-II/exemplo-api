5ead2ac47f2d94e9958e1da334896d716fa06e94
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-02-03 16:52:11 -0300

    Testando a rota de eventos

---
+++ controller/eventoController.js
@@ -0,0 +1,5 @@
+import Evento from '../model/Evento.js';
+
+export async function listarEventos(req, res){
+   console.log('OK'); 
+}
--- router/eventoRouter.js
+++ router/eventoRouter.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import { listarEventos } from '../controller/eventoController.js';
 
 const eventoRouter = express.Router();
 
-
+eventoRouter.get('/', listarEventos);
 
 export default eventoRouter;

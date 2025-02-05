1f93f22b3a5e66cc4f2967bb6de811690025b811
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-02-05 13:28:55 -0300

    Adicionando rota para salvar

--- controller/eventoController.js
+++ controller/eventoController.js
@@ -4,3 +4,9 @@ export async function listarEventos(req, res){
    const eventos = await Evento.find();
    res.json(eventos);
 }
+
+export async function criarEvento(req, res){
+   const evento = new Evento(req.body);
+   await evento.save();
+   res.status(201).json(evento);
+}
--- router/eventoRouter.js
+++ router/eventoRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { listarEventos } from '../controller/eventoController.js';
+import { listarEventos, criarEvento } from '../controller/eventoController.js';
 
 const eventoRouter = express.Router();
 
 eventoRouter.get('/', listarEventos);
+eventoRouter.post('/', criarEvento);
 
 export default eventoRouter;

81c544c742df311414946279562e98974dd11b73
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-02-05 13:58:49 -0300

    Adicionando método para deletar evento

--- controller/eventoController.js
+++ controller/eventoController.js
@@ -10,3 +10,15 @@ export async function criarEvento(req, res){
    await evento.save();
    res.status(201).json(evento);
 }
+
+export async function deletarEvento(req, res){
+   const id = req.params.id;
+   await Evento.deleteOne({_id: id}).
+      then(result => {
+         if(result.deletedCount>0){
+            res.json({message: 'Evento deletado com sucesso'});
+            return;
+         }
+      })
+   res.status(404).json({message: 'Evento não encontrado'});
+}
--- router/eventoRouter.js
+++ router/eventoRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { listarEventos, criarEvento } from '../controller/eventoController.js';
+import { listarEventos, criarEvento, deletarEvento } from '../controller/eventoController.js';
 
 const eventoRouter = express.Router();
 
 eventoRouter.get('/', listarEventos);
 eventoRouter.post('/', criarEvento);
+eventoRouter.delete('/:id', deletarEvento);
 
 export default eventoRouter;
